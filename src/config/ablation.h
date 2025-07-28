f5715e0fba149b14ba3af052051b26b82c46cb1c
Mattia Guazzaloca <mattia.guazzaloca@gmail.com>
2025-07-28 18:24:38 +0200

    fix: update Actor model input size for ONNX export and adjust ablation parameters

--- src/config/ablation.h
+++ src/config/ablation.h
@@ -1,15 +1,15 @@
 namespace learning_to_fly::config{
     struct DEFAULT_ABLATION_SPEC{
-        static constexpr bool DISTURBANCE = true;
         static constexpr bool OBSERVATION_NOISE = true;
-        static constexpr bool ASYMMETRIC_ACTOR_CRITIC = true;
         static constexpr bool ROTOR_DELAY = true;
         static constexpr bool ACTION_HISTORY = true;
         static constexpr bool ENABLE_CURRICULUM = true;
         static constexpr bool RECALCULATE_REWARDS = true;
         static constexpr bool USE_INITIAL_REWARD_FUNCTION = true;
         static constexpr bool INIT_NORMAL = true;
-        static constexpr bool EXPLORATION_NOISE_DECAY = true;
+        static constexpr bool DISTURBANCE = false;
+        static constexpr bool ASYMMETRIC_ACTOR_CRITIC = false;
+        static constexpr bool EXPLORATION_NOISE_DECAY = false;
     };
     template <typename T_ABLATION_SPEC>
     struct ABLATION_SPEC_EVAL: T_ABLATION_SPEC{
